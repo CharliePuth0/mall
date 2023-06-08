b48480d5e22c4b29ae528bd907a68561768939b6
sugerdaddy <sugerdaddy_gpt@163.com>
2023-06-09 02:21:28 +0800

    init

---
+++ mall-product/src/test/java/com/mall/product/MallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.product;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
